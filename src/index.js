766bd3bb7275783377a477c0f55c8b6980c81850
Maksym Kikot <maxym1111111@gmail.com>
2023-02-10 20:49:19 +0200

    hw done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
+import {
+  listCountryEl,
+  countryInfoEl,
+  fetchCountries,
+} from './js/fetchCountries';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+
+inputEl.addEventListener('input', debounce(inputValue, DEBOUNCE_DELAY));
+
+function inputValue(e) {
+  const value = e.target.value.trim();
+  if (value === '' || undefined) {
+    return;
+  }
+
+  listCountryEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+  fetchCountries(value);
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,56 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const listCountryEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+function fetchCountries(name) {
+  const option = 'name,capital,population,flags,languages';
+
+  const URL_FETCH = `https://restcountries.com/v3.1/name/${name}?fields=${option}`;
+
+  return fetch(URL_FETCH)
+    .then(r => r.json())
+    .then(countries => renderCountries(countries))
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderCountries(countries) {
+  if (countries.length > 10) {
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (countries.length === 1) {
+    return insertCountryInfo(countries);
+  }
+
+  insertMarcup(countries);
+}
+
+function insertCountryInfo(countries) {
+  const country = countries[0];
+  const markup = `<h1 class="main-title"><img src="${
+    country.flags.png
+  }" width="30" height ="20"> ${country.name.common}</h1>
+      <p><b>Capital:</b> ${country.capital}</p>
+      <p><b>Population:</b> ${country.population}</p>
+      <p><b>Lnguages:</b> ${Object.values(country.languages).join(', ')}</p>`;
+
+  return (countryInfoEl.innerHTML = markup);
+}
+
+function insertMarcup(countries) {
+  const marcup = countries
+    .map(country => {
+      return `<li class="item">
+          <img src="${country.flags.png}" width="30" height ="20">
+          <p class="title">${country.name.common}</p>
+        </li>`;
+    })
+    .join('');
+
+  return (listCountryEl.innerHTML = marcup);
+}
+
+export { listCountryEl, countryInfoEl, fetchCountries };
